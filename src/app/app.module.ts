345ca75ac88739465ddc152722ec9d18f146a755
Pushpendu Ghosh <pushpendu.ghosh@memeinfotech.com>
2018-02-05 11:40:25 +0530

    current chnages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TestComponent } from './test/test.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+//route configuartion
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard/:id', component: DashboardComponent },
+  { path: 'test/:type', component: TestComponent },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    // router module
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+    // other imports here
   ],
   providers: [],
   bootstrap: [AppComponent]
